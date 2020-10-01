53f9bf971ec73d0c328769ae35edc985ef8bf881
CRISPY <APrettyCoolProgram@users.noreply.github.com>
2020-09-30 21:14:06 -0400

    v0.0.44.1001

---
+++ Dungeon-TextAdventure03/Program.cs
@@ -0,0 +1,36 @@
+﻿//  ▄▄▄▄▄▄  ▄▄   ▄▄ ▄▄    ▄ ▄▄▄▄▄▄▄ ▄▄▄ ▄▄    ▄ ▄▄▄▄▄▄▄
+// █      ██  █ █  █  █  █ █       █   █  █  █ █       █
+// █  ▄    █  █ █  █   █▄█ █   ▄▄▄▄█   █   █▄█ █    ▄▄▄█
+// █ █ █   █  █▄█  █       █  █  ▄▄█   █       █   █▄▄▄
+// █ █▄█   █       █  ▄    █  █ █  █   █  ▄    █    ▄▄▄█
+// █       █       █ █ █   █  █▄▄█ █   █ █ █   █   █▄▄▄
+// █▄▄▄▄▄▄██▄▄▄▄▄▄▄█▄█  █▄▄█▄▄▄▄▄▄▄█▄▄▄█▄█  █▄▄█▄▄▄▄▄▄▄█
+
+
+
+
+
+using System.Text.Json;
+
+namespace Dungeon_TextAdventure03
+{
+    internal class Program
+    {
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="args">Passed arguments.</param>
+        private static void Main(string[] args)
+        {
+            var dungine = DungineCore.DungineObject.Build(5);
+
+            var options = new JsonSerializerOptions
+            {
+                WriteIndented = true
+            };
+
+            var jstring = JsonSerializer.Serialize(dungine, options);
+            System.IO.File.WriteAllText("test.txt", jstring);
+        }
+    }
+}
