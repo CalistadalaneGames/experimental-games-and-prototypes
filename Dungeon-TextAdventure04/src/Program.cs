f31f065efc773f9be7786258aa3f9d57ca3b0579
CRISPY <APrettyCoolProgram@users.noreply.github.com>
2020-10-02 11:11:19 -0400

    Update

---
+++ Dungeon-TextAdventure04/src/Program.cs
@@ -0,0 +1,19 @@
+﻿// Dungeon: Text Adventure 04
+// Version 0.0.0
+// https://github.com/CalistadalaneGames/experimental-games-and-prototypes
+// (C)2020 A Pretty Cool Program & Calistadalane Games All rights reserved.
+// Licensed under the Apache License v2
+
+// Program.cs: Entry point for Dungeon: Text Adventure 04
+
+using System;
+
+namespace Dungeon_TextAdventure04
+{
+    internal class Program
+    {
+        private static void Main(string[] args) => Console.WriteLine("Hello World!");
+    }
+}
+
+// ⠉⠕⠏⠽⠗⠊⠛⠓⠞⠼⠃⠼⠚⠼⠃⠼⠚⠁⠏⠗⠑⠞⠞⠽⠉⠕⠕⠇⠏⠗⠕⠛⠗⠁⠍⠁⠝⠙⠉⠁⠇⠊⠎⠞⠁⠙⠁⠇⠁⠝⠑⠛⠁⠍⠑⠎
