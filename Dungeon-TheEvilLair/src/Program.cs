acabb26493fb8ad7ae1de88fc42dec82bd3ee014
Chris Banwarth <APrettyCoolProgram@users.noreply.github.com>
2020-10-08 11:17:19 -0400

    Dungeon-TheEvilLair update

---
+++ Dungeon-TheEvilLair/src/Program.cs
@@ -0,0 +1,27 @@
+﻿/* ⠉⠕⠏⠽⠗⠊⠛⠓⠞⠼⠃⠼⠚⠼⠃⠼⠚⠁⠏⠗⠑⠞⠞⠽⠉⠕⠕⠇⠏⠗⠕⠛⠗⠁⠍⠁⠝⠙⠉⠁⠇⠊⠎⠞⠁⠙⠁⠇⠁⠝⠑⠛⠁⠍⠑⠎
+ * Dungeon: The Evil Lair
+ * A prototype text-based game.
+ * https://github.com/CalistadalaneGames/experimental-games-and-prototypes
+ * Version X.x.x.x
+ * (C)2020 A Pretty Cool Program & Calistadalane Games.
+ * All rights reserved.
+ * Licensed under the Apache License v2 [https://apache.org/licenses/LICENSE-2.0]
+ * 
+ * Uses the Dungine game engine: https://github.com/CalistadalaneGames/dungine
+ * 
+ * FILE:
+ * DESC:
+ * AUTHOR(S): development@aprettycoolprogram.com      
+ */
+using System;
+
+namespace Dungeon_TheEvilLair
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
