4200853308b79cf85ff38f28d6ba953d14c43a32
Chris Banwarth <APrettyCoolProgram@users.noreply.github.com>
2020-10-07 20:36:31 -0400

    Update

--- Dungeon-TextAdventure04/src/Program.cs
+++ Dungeon-TextAdventure04/src/Program.cs
@@ -1,10 +1,29 @@
-﻿// Dungeon: Text Adventure 04
-// Version 0.0.0
-// https://github.com/CalistadalaneGames/experimental-games-and-prototypes
-// (C)2020 A Pretty Cool Program & Calistadalane Games All rights reserved.
-// Licensed under the Apache License v2
+﻿/*  ⠉⠕⠏⠽⠗⠊⠛⠓⠞⠼⠃⠼⠚⠼⠃⠼⠚⠁⠏⠗⠑⠞⠞⠽⠉⠕⠕⠇⠏⠗⠕⠛⠗⠁⠍⠁⠝⠙⠉⠁⠇⠊⠎⠞⠁⠙⠁⠇⠁⠝⠑⠛⠁⠍⠑⠎
+▀███▀▀▀██▄ ▀███▀   ▀███▀███▄   ▀███▀ ▄▄█▀▀▀█▄█▀███▀▀▀███  ▄▄█▀▀██▄ ▀███▄   ▀███▀
+  ██    ▀██▄██       █   ███▄    █ ▄██▀     ▀█  ██    ▀█▄██▀    ▀██▄ ███▄    █  
+  ██     ▀████       █   █ ███   █ ██▀       ▀  ██   █  ██▀      ▀██ █ ███   █  
+  ██      ████       █   █  ▀██▄ █ █▓           ██████  ██        ██ █  ▀██▄ █  
+  █▓     ▄████       ▓   █   ▀██▄▓ █▓▄    ▀████ ██   █  ▄█        ██ █   ▀██▄▓  
+  █▓    ▄█▓▀██       ▓   ▓     ▓█▓ ▀▓█▄     ██  █▓     ▄███      ██▀ ▓     ▓█▓  
+  ▓▓     ▓▓▓▓█       ▓   ▓   ▀▓▓▓▓ ▓▓▓    ▀▓█▓▓ ▓▓   ▓  ▓██      ▓█▓ ▓   ▀▓▓▓▓  
+  ▓▒    ▓▓▒▀▓▓▓     ▓▓   ▓     ▓▓▓ ▀▒▓▓     ▓▓  ▓▓     ▓▓█▓▓▓    ▓▓▓ ▓     ▓▓▓ 
+▒ ▒ ▒ ▒ ▒    ▒ ▒ ▒ ▒▓▒ ▒ ▒ ▒    ▒▓▓  ▒▒▒ ▒ ▒▒ ▒ ▒▒▒▓▒ ▒ ▒ ▒ ▒ ▒ ▒  ▒ ▒ ▒    ▒▓▓ 
 
-// Program.cs: Entry point for Dungeon: Text Adventure 04
+▀█▀ ██▀ ▀▄▀ ▀█▀   ▄▀▄ █▀▄ █ █ ██▀ █▄ █ █ █ █▀▄ ██▀   █▀█ █▄
+ █  █▄▄ █ █  █    █▀█ █▄▀ ▀▄▀ █▄▄ █ ▀█ ▀▄█ █▀▄ █▄▄   █▄█  █
+       
+    A text adventure
+    https://github.com/CalistadalaneGames/dungeon
+    Version x.x.x
+
+    (C)2020 A Pretty Cool Program & Calistadalane Games.
+    All rights reserved.
+    Licensed under the Apache License v2 [https://apache.org/licenses/LICENSE-2.0]
+
+    FILE:
+    DESC:
+    AUTHOR(S): development@aprettycoolprogram.com
+*/
 
 namespace Dungeon_TextAdventure04
 {
